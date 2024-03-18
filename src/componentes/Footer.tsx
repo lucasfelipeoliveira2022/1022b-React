5b070cf04e32dffb62b9c5f12920db65f6db3fce
lucasfelipeoliveira2022 <lucas.lima8@estudante.ifms.edu.br>
2024-03-18 11:44:00 -0400

    componentesheadermainfooter

---
+++ src/componentes/Footer.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import './Footer.css'
+
+function Footer(){
+    return (
+        <footer className='top3'>Footer</footer>
+    )
+} 
+
+export default Footer;
---
+++ src/componentes/Main.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import './Main.css'
+
+function Main(){
+    return (
+        <main className='top2'>Main</main>
+    )
+} 
+
+export default Main;
